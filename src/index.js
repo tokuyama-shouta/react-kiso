0eb8f83aaba7b3c74490ac0ffe34a4328193e4cc
tokuyama shouta <toku1031ts@gmail.com>
2021-04-12 20:49:34 +0900

    JSX

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-import Blog from './Blog'
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </React.Fragment>
+    
+  );
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Blog />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<App/>,document.getElementById("root"));
