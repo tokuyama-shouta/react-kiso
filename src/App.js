bf3008aa89382e7055b0ad3f5791c3d1eb666618
tokuyama shouta <toku1031ts@gmail.com>
2021-04-13 20:12:33 +0900

    component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <React.Fragment>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </React.Fragment>
+    
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
-const App = () => {
-  return (
-    <React.Fragment>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </React.Fragment>
-    
-  );
-};
+
 
 ReactDom.render(<App/>,document.getElementById("root"));
