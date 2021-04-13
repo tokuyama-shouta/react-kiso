cb3f9be260ee39bc13494b8004b5aef88ca01a84
tokuyama shouta <toku1031ts@gmail.com>
2021-04-13 20:18:34 +0900

    event style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const App = () => {
+  const onClickButton = () => alert();
   return(
     <React.Fragment>
       <h1>こんにちは！</h1>
       <p>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </React.Fragment>
     
   );
