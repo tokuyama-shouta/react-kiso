fa3b685df92615cea88a7542194655222bc5e392
tokuyama shouta <toku1031ts@gmail.com>
2021-04-18 16:14:51 +0900

    完了

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,15 @@ export const App = () => {
     setIncompleteTodos(newTodos);
   }
 
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    const newCompleteTodos = [...completeTodos,incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  
+  }
+
   return (
     <>
     <div>
@@ -37,7 +46,7 @@ export const App = () => {
           return (
             <div key={todo}>
               <li>{todo}</li>
-              <button>完了</button>
+              <button onClick={() => onClickComplete(index)}>完了</button>
               <button onClick={() => onClickDelete(index)}>削除</button>
             </div>
           );
